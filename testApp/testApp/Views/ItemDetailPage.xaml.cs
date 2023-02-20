09100e910da78f9c5ede12f4956b8f432af57f1b
Mutai Patrick <kipkiruimutaipatrick@gmail.com>
2023-02-20 13:44:55 +0300

    first commit...

---
+++ testApp/testApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using testApp.ViewModels;
+using Xamarin.Forms;
+
+namespace testApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
